ecb849b371c52fe6407c97713642433c76b23544
saintsHr <hiqkrs@gmail.com>
2025-11-15 14:36:17 -0300

    nothing lol

--- src/kernel.c
+++ src/kernel.c
@@ -1,3 +1,16 @@
-void kmain(void){
+#include "vga.h"
+#include "util.h"
 
+void kmain(void){
+    unsigned long long i = 0;
+
+    vga_moveCursor(39, 12);
+
+    while(1){
+        vga_print(VGA_BLACK, VGA_LGRAY, util_intToStr(i));
+        util_wait(1000);
+        vga_clearScreen();
+        i++;
+        vga_moveCursor(39, 12);
+    }
 }
